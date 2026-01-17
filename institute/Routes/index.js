7ea3c4f05a69387b20edfe4d3ff539a64334cce3
Manaliskillsconnect-26 <manali.skillsconnect@gmail.com>
2026-01-17 10:28:34 +0530

    Delete institute directory

--- institute/Routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import instituteRoutes from "./v1/instituteRoutes.js";
-
-const router = express.Router();
-
-router.use("/v1/institute", instituteRoutes);
-
-export default router;
